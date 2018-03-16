0ec11efdb255c5d7c323c761368c15927e80612e
QQQQQby <yx41_qby@126.com>
2018-03-16 13:20:02 +0800

    b

--- app/src/main/java/com/example/hp/calculator/MainActivity.java
+++ app/src/main/java/com/example/hp/calculator/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.hp.calculator;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
     }
 }
